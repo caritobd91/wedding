037b537b2f0f595b9ce838c295b4b603971a550c
Clayton Orman <Clayton@Claytons-MacBook-Pro.local>
2017-05-25 16:44:32 -0400

    Remove unneccessary code

--- Connection.php
+++ Connection.php
@@ -1,19 +1,3 @@
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:carolinaclaytonwedding.database.windows.net,1433; Database = Wedding", "orman112", "{your_password_here}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "orman112@carolinaclaytonwedding", "pwd" => "{your_password_here}", "Database" => "Wedding", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:carolinaclaytonwedding.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-
-
 <?php
 $serverName = SQLAZURECONNSTR_serverName;
 $Uid = SQLAZURECONNSTR_UID;
