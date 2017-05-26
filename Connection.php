3404b20df604f89dc82c40a29a4341645a9e4b52
Clayton Orman <corman.dev@gmail.com>
2017-05-26 15:42:07 -0400

    Add HTML Table

--- Connection.php
+++ Connection.php
@@ -38,8 +38,23 @@ else {
      die(var_dump($e));
  }
 
-while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-    echo ($row['FirstName'] . " " . $row['LastName'] . " " . $row['HasGuest'] . "<br />");
-}
-sqlsrv_free_stmt($getResults);
+$sql_select = "SELECT * FROM [Attendee]";
+ $stmt = $conn->query($sql_select);
+ $registrants = $stmt->fetchAll(); 
+ if(count($registrants) > 0) {
+     echo "<h2>People who are registered:</h2>";
+     echo "<table>";
+     echo "<tr><th>First Name</th>";
+     echo "<th>Last Name</th>";
+     echo "<th>Is Attending</th></tr>";
+     foreach($registrants as $registrant) {
+         echo "<tr><td>".$registrant['FirstName']."</td>";
+         echo "<td>".$registrant['LastName']."</td>";
+         echo "<td>".$registrant['IsComing']."</td></tr>";
+     }
+      echo "</table>";
+ } else {
+     echo "<h3>No one is currently attending.</h3>";
+ }
 ?>
+
