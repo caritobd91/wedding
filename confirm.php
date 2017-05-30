80e26108780e816050af6c74723dcee624716aa9
Clayton Orman <corman.dev@gmail.com>
2017-05-30 11:43:57 -0400

    Update SQL statement

--- confirm.php
+++ confirm.php
@@ -1,29 +1,43 @@
 <?php
+/* SQL Connection */
+$host = getenv("SQLAZURECONNSTR_serverName");
+$user = getenv("SQLAZURECONNSTR_UID");
+$pwd = getenv("SQLAZURECONNSTR_password");
+$db = "Wedding";
+
+try {
+    $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch(Exception $e){
+    die(var_dump($e));
+}
+/* SQL Connection */
+
  if(!empty($_POST)) {
  try {
      $firstName = $_POST['FirstName'];
-     $lasttName = $_POST['LastName'];
-     $email = $_POST['Email'];
+     $lastName = $_POST['LastName'];
      $isComing = $_POST['RSVP'];
      $message = $_POST['Message'];
      // Insert data
-     /*$sql_insert = "INSERT INTO registration_tbl (name, email, date) 
-                    VALUES (?,?,?)";
+     $sql_insert = "INSERT INTO Attendee (firstName, lastName, isComing, message) 
+                    VALUES (?,?,?,?)";
      $stmt = $conn->prepare($sql_insert);
-     $stmt->bindValue(1, $name);
-     $stmt->bindValue(2, $email);
-     $stmt->bindValue(3, $date);
-     $stmt->execute();*/
+     $stmt->bindValue(1, $firstName);
+     $stmt->bindValue(2, $lastName);
+     $stmt->bindValue(3, $isComing);
+     $stmt->bindValue(4, $message);
+     $stmt->execute();
 
      echo "First: $firstName </br>";
      echo "Last: $lastName </br>";
-     echo "Email: $email </br>";
      echo "Coming?: $isComing </br>";
      echo "Comments: $message </br>";
  }
  catch(Exception $e) {
      die(var_dump($e));
  }
- echo "<h3>Thanks for Confirming!</h3>";
+ echo "<h3>Thanks for confirming, $firstName!</h3>";
  }
 ?>
