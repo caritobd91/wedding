cd8bc6c417ae396ebaa3996042fb87b18fcf9d6d
Clayton Orman <corman.dev@gmail.com>
2017-06-13 12:30:41 -0400

    Update confirmation page

--- confirm.php
+++ confirm.php
@@ -1,6 +1,6 @@
 <?php
 /* SQL Connection */
-/*
+
 $host = getenv("SQLAZURECONNSTR_serverName");
 $user = getenv("SQLAZURECONNSTR_UID");
 $pwd = getenv("SQLAZURECONNSTR_password");
@@ -12,7 +12,7 @@ try {
 }
 catch(Exception $e){
     die(var_dump($e));
-}*/
+}
 /* SQL Connection */
 
  if(!empty($_POST)) {
@@ -23,13 +23,14 @@ catch(Exception $e){
      $message = $_POST['Message'];
      // Insert data
      $sql_insert = "INSERT INTO Attendee (firstName, lastName, isComing, message) 
-                    VALUES (?,?,?,?)";
+                    VALUES (_firstName, _lastName, _isComing, _message)";
      $stmt = $conn->prepare($sql_insert);
-     $stmt->bindValue(1, $firstName);
-     $stmt->bindValue(2, $lastName);
-     $stmt->bindValue(3, $isComing);
-     $stmt->bindValue(4, $message);
-     $stmt->execute();
+     $stmt->bindValue('_firstName', $firstName);
+     $stmt->bindValue('_lastName', $lastName);
+     $stmt->bindValue('_isComing', $isComing);
+     $stmt->bindValue('_message', $message);
+     echo "STMT: $stmt";
+     /*$stmt->execute();*/
 
      echo "First: $firstName </br>";
      echo "Last: $lastName </br>";
