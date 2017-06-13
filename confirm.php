c576f82d580e2c26f27977ac3defc17aa7c9823b
Clayton Orman <corman.dev@gmail.com>
2017-06-13 11:58:51 -0400

    Update confirmation page

--- confirm.php
+++ confirm.php
@@ -17,13 +17,16 @@ catch(Exception $e){
 
  if(!empty($_POST)) {
  try {
+
+     //date_default_timezone_set('UTC');
      $firstName = $_POST['FirstName'];
      $lastName = $_POST['LastName'];
      $isComing = ($_POST['RSVP'] == 'Yes' ? 1 : 0);
      $message = $_POST['Message'];
+     $dateRegistered = date('m/d/Y');
      // Insert data
-     /*$sql_insert = "INSERT INTO Attendee (firstName, lastName, isComing, message) 
-                    VALUES (?,?,?,?)";
+     /*$sql_insert = "INSERT INTO Attendee (firstName, lastName, isComing, message, dateRegistered) 
+                    VALUES (?,?,?,?, TO_DATE(?, 'DD/MM/YYYY'))";
      $stmt = $conn->prepare($sql_insert);
      $stmt->bindValue(1, $firstName);
      $stmt->bindValue(2, $lastName);
@@ -35,6 +38,7 @@ catch(Exception $e){
      echo "Last: $lastName </br>";
      echo "Coming?: $isComing </br>";
      echo "Comments: $message </br>";
+     echo "Date Registered: $dateRegistered </br>";
  }
  catch(Exception $e) {
      die(var_dump($e));
