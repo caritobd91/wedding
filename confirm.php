bb82508f50238bf558de2939dbd06471edd48010
Clayton Orman <corman.dev@gmail.com>
2017-06-13 12:15:00 -0400

    Update confirmation page

--- confirm.php
+++ confirm.php
@@ -25,7 +25,7 @@ catch(Exception $e){
      $message = $_POST['Message'];
      $dateRegistered = date('m/d/Y');
      // Insert data
-     $sql_insert = "INSERT INTO Attendee (firstName, lastName, isComing, message) 
+     /*$sql_insert = "INSERT INTO Attendee (firstName, lastName, isComing, message) 
                     VALUES (?,?,?,?)";
      $stmt = $conn->prepare($sql_insert);
      $stmt->bindValue(1, $firstName);
