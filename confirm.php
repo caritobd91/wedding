45cf70f23b4039ba76ad6154956134e172d81bee
Clayton Orman <corman.dev@gmail.com>
2017-06-13 13:13:20 -0400

    Update confirmation page

--- confirm.php
+++ confirm.php
@@ -23,16 +23,32 @@ catch(Exception $e){
      $message = $_POST['Message'];
      $dateRegistered = date('m/d/Y');
 
-     // Insert data
-     $sql_insert = "INSERT INTO Attendee (firstName, lastName, isComing, message, dateRegistered) 
-                    VALUES (:firstName, :lastName, :isComing, :message, :dateRegistered)";
-     $stmt = $conn->prepare($sql_insert);
+     $sql_select = "SELECT TOP 1* FROM Attendee WHERE FirstName = :firstName AND LastName = :lastName";
+     $stmt = $conn->prepare($sql_select);
      $stmt->bindValue(':firstName', $firstName);
      $stmt->bindValue(':lastName', $lastName);
-     $stmt->bindValue(':isComing', $isComing);
-     $stmt->bindValue(':message', $message);
-     $stmt->bindValue(':dateRegistered', $dateRegistered);
      $stmt->execute();
+
+    if(!($stmt->rowCount() > 0))
+    {
+        // Insert data
+        $sql_insert = "INSERT INTO Attendee (firstName, lastName, isComing, message, dateRegistered) 
+                        VALUES (:firstName, :lastName, :isComing, :message, :dateRegistered)";
+        $stmt = $conn->prepare($sql_insert);
+        //$stmt->bindValue(':firstName', $firstName);
+        //$stmt->bindValue(':lastName', $lastName);
+        $stmt->bindValue(':isComing', $isComing);
+        $stmt->bindValue(':message', $message);
+        $stmt->bindValue(':dateRegistered', $dateRegistered);
+        $stmt->execute();
+
+        echo "<h3>Thanks for confirming on $dateRegistered, $firstName!</h3>";
+    }
+    else {
+        $result = $stmt -> fetch(PDO::FETCH_ASSOC);
+        $previousDate = $result['dateRegistered'];
+        echo "<h3>You've already registered on  $firstName</h3>";
+    }
 
      echo "First: $firstName </br>";
      echo "Last: $lastName </br>";
@@ -43,6 +59,5 @@ catch(Exception $e){
      die(var_dump($e));
      error_log( "Error: $e" );
  }
- echo "<h3>Thanks for confirming on $dateRegistered, $firstName!</h3>";
  }
 ?>
